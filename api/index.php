8cd179c22fc0dbe37ad14a623a4b6ba7d9f690a4
wildan <wildansetiawan425@gmail.com>
2025-06-24 12:51:12 +0700

    first

--- api/index.php
+++ api/index.php
@@ -1,4 +1,4 @@
 <?php
 // File: /api/index.php
 // Memuat file index.php publik dari Laravel sebagai 'serverless function'.
-require _DIR_ . '/../public/index.php';
+require __DIR__ . '/../public/index.php';
